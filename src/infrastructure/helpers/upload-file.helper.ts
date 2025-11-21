412cd6f8bf6d18fed174d69bb095014f648df349
dothinh115 <dothinh115@gmail.com>
2025-11-21 11:13:06 +0700

    refactor: remove createDeleteFileHelper method from UploadFileHelper
    - Removed the createDeleteFileHelper method to streamline the UploadFileHelper class and eliminate unused code.

--- src/infrastructure/helpers/upload-file.helper.ts
+++ src/infrastructure/helpers/upload-file.helper.ts
@@ -247,31 +247,5 @@ export class UploadFileHelper {
     };
   }
 
-  createDeleteFileHelper(context: TDynamicContext) {
-    return async (fileId: string | number) => {
-      try {
-        const fileRepo = this.getFileRepo(context);
-
-        const files = await fileRepo.find({ where: { id: { _eq: fileId } } });
-        const file = files.data?.[0];
-
-        if (!file) {
-          throw new Error(`File with ID ${fileId} not found`);
-        }
-
-        const { location, storageConfig } = file;
-
-        await this.fileManagementService.deletePhysicalFile(
-          location,
-          storageConfig?.id || null,
-        );
-
-        return await fileRepo.delete({ id: fileId });
-      } catch (error: any) {
-        this.handleError(error, 'deleteFile');
->>>>>>> Stashed changes
-      }
-    };
-  }
 }
 
